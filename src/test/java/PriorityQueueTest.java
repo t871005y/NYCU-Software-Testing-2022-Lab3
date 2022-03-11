5e8a1102b0053204804a1eddbc23b0736a77817d
YC_Lin <t871005y@gmail.com>
2022-03-11 21:33:09 +0800

    success

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider() {
         return Stream.of(
-                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{1, 3, 4, 5}),
+                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5}),
                 Arguments.of(new int[]{1, 2, 3, 4}, new int[]{1, 2, 3, 4}),
                 Arguments.of(new int[]{-3, -2, 1, 5}, new int[]{-3, -2, 1, 5}),
                 Arguments.of(new int[]{0, 2, -5, -4}, new int[]{-5, -4, 0, 2}),
